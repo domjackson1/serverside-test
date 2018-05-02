35c649baee4723a2fc27148ce92d4ab5ab59555b
Dom Jackson <domjackson94@gmail.com>
2018-05-02 23:50:38 +0100

    Add Application.java main command line app class

---
+++ src/main/java/io/github/domjackson1/groceryscraper/Application.java
@@ -0,0 +1,18 @@
+package io.github.domjackson1.groceryscraper;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("I am a command line Spring Boot application");
+    }
+}
