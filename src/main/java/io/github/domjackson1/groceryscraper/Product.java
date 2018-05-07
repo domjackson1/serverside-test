def4f8d68c950cdb7a66c1aa6f3c4412ca82f37c
Dom Jackson <domjackson94@gmail.com>
2018-05-07 13:07:47 +0100

    Added overridden toStrnig to Product to print JSON

--- src/main/java/io/github/domjackson1/groceryscraper/Product.java
+++ src/main/java/io/github/domjackson1/groceryscraper/Product.java
@@ -1,10 +1,17 @@
 package io.github.domjackson1.groceryscraper;
 
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Product {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     private String title;
     private String description;
@@ -49,5 +56,20 @@ public class Product {
 
     public void setKcalPer100Grams(int kcalPer100Grams) {
         this.kcalPer100Grams = kcalPer100Grams;
+    }
+
+    @Override
+    public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        String jsonString = "";
+        try {
+            mapper.configure(SerializationConfig.Feature.INDENT_OUTPUT, true);
+            jsonString = mapper.writeValueAsString(this);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+        }
+
+        return jsonString;
     }
 }
